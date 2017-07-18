efefcfa06f2e462c5e1837f9d7252b4bb4212a7d
awethemes <awethemes@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2017-07-18 10:45:41 +0000

    Update latest version
    git-svn-id: http://plugins.svn.wordpress.org/awebooking/trunk@1698082 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ inc/Support/Multilingual.php
@@ -0,0 +1,92 @@
+<?php
+namespace AweBooking\Support;
+
+class Multilingual {
+	/**
+	 * The active language.
+	 *
+	 * @var string
+	 */
+	protected $active_language;
+
+	/**
+	 * The "main" language.
+	 *
+	 * @var string
+	 */
+	protected $main_language;
+
+	/**
+	 * An array of all available languages.
+	 *
+	 * @var array
+	 */
+	protected $available_languages = [];
+
+	/**
+	 * Class constructor.
+	 */
+	public function __construct() {
+	}
+
+	/**
+	 * Get the main language.
+	 *
+	 * @return string|null
+	 */
+	public function get_active_language() {
+		if ( $this->active_language ) {
+			return $this->active_language;
+		}
+
+		if ( $this->is_wpml() ) {
+			global $sitepress;
+			$this->active_language = $sitepress->get_current_language();
+		}
+
+		// TODO: ...
+		// Polylang support...
+
+		return $this->active_language;
+	}
+
+	/**
+	 * Get the main language.
+	 *
+	 * @return string|null
+	 */
+	public function get_default_language() {
+		if ( $this->main_language ) {
+			return $this->main_language;
+		}
+
+		if ( $this->is_wpml() ) {
+			global $sitepress;
+			$this->main_language = $sitepress->get_default_language();
+		} elseif ( $this->is_polylang() ) {
+			$this->main_language = pll_default_language( 'slug' );
+		}
+
+		return $this->main_language;
+	}
+
+	/**
+	 * Determine if we're using PolyLang.
+	 *
+	 * @return boolean
+	 */
+	public function is_polylang() {
+		return defined( 'POLYLANG_VERSION' );
+	}
+
+	/**
+	 * Determine if we're using WPML.
+	 *
+	 * Since PolyLang has a compatibility layer for WPML, we'll have to consider that too.
+	 *
+	 * @return boolean
+	 */
+	public function is_wpml() {
+		return ( defined( 'ICL_SITEPRESS_VERSION' ) && ! $this->is_polylang() );
+	}
+}
