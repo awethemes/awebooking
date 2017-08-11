f8148e61fece075c370bc581d96910aae77f72bb
Nguyen Van Anh <anhskohbo@gmail.com>
2017-08-11 15:20:56 +0700

    Make travis success

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -25,9 +25,7 @@ require_once $_tests_dir . '/includes/functions.php';
  * Manually load the plugin being tested.
  */
 tests_add_filter( 'muplugins_loaded', function () {
-	// Load Skeleton (and CMB2) by manually.
-	// require_once dirname( __DIR__ ) . '/vendor/webdevstudios/cmb2/bootstrap.php';
-	require_once dirname( __DIR__ ) . '/vendor/awethemes/skeleton/skeleton.php';
+	require_once dirname( __DIR__ ) . '/vendor/autoload.php';
 
 	require dirname( __DIR__ ) . '/awebooking.php';
 
