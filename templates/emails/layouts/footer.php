92dd1f8928ea98a29161673075e2dc63937eb9dc
HoangHuu <huuhoangcd221@gmail.com>
2017-09-01 17:52:34 +0700

    Fix footer email template

--- templates/emails/layouts/footer.php
+++ templates/emails/layouts/footer.php
@@ -3,8 +3,7 @@
 		<table class="footer" align="center" width="570" cellpadding="0" cellspacing="0">
 			<tr>
 				<td class="content-cell" align="center">
-					<?php $copyright = sprintf( esc_html__( '&copy; %1$s %2$s. All rights reserved.', 'awebooking' ), date( 'Y' ), get_bloginfo( 'name' ) );
-					echo esc_html( apply_filters( 'awebooking/email_footer', $copyright ) ); ?>
+					<?php echo esc_html( awebooking_option( 'email_copyright' ) ); ?>
 				</td>
 			</tr>
 		</table>
