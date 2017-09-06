8161726e5b65ee4b5d56252085e2feee31f09bb7
Nguyen Van Anh <anhskohbo@gmail.com>
2017-09-06 10:21:12 +0700

    Correct way to load skeleton

--- bootstrap.php
+++ bootstrap.php
@@ -6,7 +6,7 @@
  */
 
 // We require our framework if needed.
-if ( ! defined( 'SKELETON_LOADED' ) ) {
+if ( ! defined( 'AWETHEMES_SKELETON_LOADED' ) ) {
 	if ( file_exists( __DIR__ . '/skeleton/skeleton.php' ) ) {
 		require_once trailingslashit( __DIR__ ) . '/skeleton/skeleton.php';
 	} elseif ( file_exists( __DIR__ . '/vendor/awethemes/skeleton/skeleton.php' ) ) {
