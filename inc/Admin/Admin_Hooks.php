23f8ed76d3047ffcc2124ad9c7dbe3e043762aff
Nguyen Van Anh <anhskohbo@gmail.com>
2017-09-27 10:01:31 +0700

    Singleton

--- inc/Admin/Admin_Hooks.php
+++ inc/Admin/Admin_Hooks.php
@@ -22,11 +22,11 @@ class Admin_Hooks extends Service_Hooks {
 	 * @param Container $container Container instance.
 	 */
 	public function register( $container ) {
-		$container->bind( 'admin_notices', function() {
+		$container->singleton( 'admin_notices', function() {
 			return new Admin_Notices;
 		});
 
-		$container->bind( 'admin_menu', function() {
+		$container->singleton( 'admin_menu', function() {
 			return new Admin_Menu;
 		});
 	}
