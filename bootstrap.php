6fb7e811eb513a7d3687954c6e8b12a0bd19c094
Nguyen Van Anh <anhskohbo@gmail.com>
2017-10-02 18:22:14 +0700

    Added missing skeleton_psr4_autoloader

--- bootstrap.php
+++ bootstrap.php
@@ -30,3 +30,27 @@ class_alias( 'AweBooking\\AweBooking', 'AweBooking' );
 // TODO: Remove this in next version.
 class_alias( 'AweBooking\Support\WP_Object', 'AweBooking\Model\WP_Object' );
 class_alias( 'AweBooking\Support\Service_Hooks', 'Skeleton\Container\Service_Hooks' );
+
+if ( ! function_exists( 'skeleton_psr4_autoloader' ) ) :
+	/**
+	 * Register PSR-4 autoload classess.
+	 *
+	 * @param  string|array $namespace A string of namespace or an array with
+	 *                                 namespace and directory to autoload.
+	 * @param  string       $base_dir  Autoload directory if $namespace is string.
+	 * @return void
+	 */
+	function skeleton_psr4_autoloader( $namespace, $base_dir = null ) {
+		$loader = new Composer\Autoload\ClassLoader;
+
+		if ( is_string( $namespace ) && $base_dir ) {
+			$loader->setPsr4( rtrim($namespace, '\\') . '\\', $base_dir );
+		} elseif ( is_array( $namespace ) ) {
+			foreach ( $namespace as $prefix => $dir ) {
+				$loader->setPsr4( rtrim($prefix, '\\') . '\\', $dir );
+			}
+		}
+
+		$loader->register( true );
+	}
+endif;
