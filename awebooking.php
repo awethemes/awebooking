4941200d23909f80db06a71897ff70a0b2e731d2
Nguyen Van Anh <anhskohbo@gmail.com>
2017-10-13 09:47:09 +0700

    beta9-dev

--- awebooking.php
+++ awebooking.php
@@ -7,7 +7,7 @@
  * Author URI:      https://awethemes.com
  * Text Domain:     awebooking
  * Domain Path:     /languages
- * Version:         3.0.0-beta8
+ * Version:         3.0.0-beta9-dev
  *
  * @package         AweBooking
  */
