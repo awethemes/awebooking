0fee4e985f0bd94504e695c9adf521064ef336d3
Nguyen Van Anh <anhskohbo@gmail.com>
2018-01-12 09:57:09 +0700

    Fixed missing magnific popup

--- inc/Providers/Frontend_Service_Provider.php
+++ inc/Providers/Frontend_Service_Provider.php
@@ -41,13 +41,11 @@ class Frontend_Service_Provider extends Service_Provider {
 		wp_enqueue_style( 'awebooking-template', awebooking()->plugin_url() . '/assets/css/awebooking.css', array(), AWEBOOKING_VERSION );
 		wp_register_style( 'magnific-popup', awebooking()->plugin_url() . '/assets/css/magnific-popup.css', array(), '1.1.0' );
 
-		wp_enqueue_style( 'magnific-popup' );
-
 		wp_enqueue_script( 'jquery-ui-accordion' );
 		wp_enqueue_script( 'jquery-ui-datepicker' );
+
 		wp_enqueue_script( 'awebooking', awebooking()->plugin_url() . '/assets/js/front-end/awebooking.js', array( 'jquery' ), AWEBOOKING_VERSION, true );
 		wp_register_script( 'magnific-popup', awebooking()->plugin_url() . '/assets/js/magnific-popup/jquery.magnific-popup.min.js', array( 'jquery' ), '1.1.0', true );
-		wp_enqueue_script( 'magnific-popup' );
 
 		wp_enqueue_script( 'booking-ajax', awebooking()->plugin_url() . '/assets/js/front-end/booking-handler.js', array( 'jquery' ), AWEBOOKING_VERSION, true );
 		wp_localize_script( 'booking-ajax', 'booking_ajax', array(
