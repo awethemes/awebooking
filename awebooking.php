252c3762094a2d283ca863c44f22d8f3844c09f5
Nguyen Van Anh <anhskohbo@gmail.com>
2018-01-12 15:33:30 +0700

    v3.0.0-rc1

--- awebooking.php
+++ awebooking.php
@@ -7,7 +7,7 @@
  * Author URI:      https://awethemes.com
  * Text Domain:     awebooking
  * Domain Path:     /languages
- * Version:         3.0-rc1
+ * Version:         3.0.0-rc1
  *
  * @package         AweBooking
  */
--- inc/AweBooking.php
+++ inc/AweBooking.php
@@ -17,7 +17,7 @@ final class AweBooking extends Container {
 	 *
 	 * @var string
 	 */
-	const VERSION = '3.0-rc1';
+	const VERSION = '3.0.0-rc1';
 
 	/* Deprecated constants */
 	const SETTING_KEY    = 'awebooking_settings';
