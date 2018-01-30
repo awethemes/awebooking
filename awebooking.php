db5ba3bb046e6e2caf42af35e172f5a58072e4e7
Nguyen Van Anh <anhskohbo@gmail.com>
2018-01-30 15:32:53 +0700

    prepare 3.0.4

--- awebooking.php
+++ awebooking.php
@@ -7,7 +7,7 @@
  * Author URI:      https://awethemes.com
  * Text Domain:     awebooking
  * Domain Path:     /languages
- * Version:         3.0.3
+ * Version:         3.0.4
  *
  * @package         AweBooking
  */
