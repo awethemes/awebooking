d02996b2540f8c72bdd7695385edf1b8f1cd6794
HoangHuu <huuhoangcd221@gmail.com>
2018-02-10 11:27:46 +0700

    Remove: add source feature

--- inc/Admin/views/sources/manager.php
+++ inc/Admin/views/sources/manager.php
@@ -17,9 +17,6 @@ $primary_sources = (array) awebooking()->make( 'primary_sources' );
 				<th style="width: 65px;"></th>
 				<th style="width: 20%;"><span><?php esc_html_e( 'Sources', 'awebooking' ); ?></span></th>
 				<th><span><?php esc_html_e( 'Taxes / Fees', 'awebooking' ); ?></span></th>
-				<th style="text-align: right;">
-					<a href="#awebooking-create-source" class="button" data-toggle="awebooking-popup"><?php esc_html_e( 'New Source', 'awebooking' ); ?></a>
-				</th>
 			</tr>
 		</thead>
 
@@ -44,17 +41,3 @@ $primary_sources = (array) awebooking()->make( 'primary_sources' );
 
 	<input type="submit" name="submit-cmb" class="button button-primary" value="<?php echo esc_html__( 'Save changes', 'awebooking' ); ?>">
 </form>
-
-<div id="awebooking-create-source" title="<?php esc_html_e( 'New Reservation Source', 'awebooking' ); ?>" class="hidden" style="display: none;">
-	<form method="POST" action="<?php echo esc_url( awebooking( 'url' )->admin_route( 'sources' ) ); ?>">
-		<?php wp_nonce_field( 'create_reservation_source', '_wpnonce', true ); ?>
-
-		<div class="awebooking-dialog-contents">
-			<?php ( new New_Source_Form )->output(); ?>
-		</div>
-
-		<div class="awebooking-dialog-buttons">
-			<button class="button button-primary" type="submit"><?php esc_html_e( 'Add New Source', 'awebooking' ); ?></button>
-		</div>
-	</form>
-</div>
