37186d9df3ed38411b91b545070fecfd1d6c1712
HoangHuu <huuhoangcd221@gmail.com>
2018-05-11 14:39:10 +0700

    Remove new reservation.

--- inc/Admin/views/calendar/index.php
+++ inc/Admin/views/calendar/index.php
@@ -1,6 +1,5 @@
 <div class="wrap">
 	<h1 class="wp-heading-inline"><?php esc_html_e( 'Calendar', 'awebooking' ); ?></h1>
-	<a href="<?php echo esc_url( awebooking( 'url' )->admin_route( 'reservation' ) ); ?>" class="page-title-action"><?php echo esc_html__( 'New Reservation', 'awebooking' ); ?></a>
 	<hr class="wp-header-end">
 
 	<div class="abrs-toolbar abrs-search-toolbar dp-flex">
