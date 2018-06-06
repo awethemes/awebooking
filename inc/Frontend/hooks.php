d0b97d5f4f70ee7da11b079ba6f524ad1448eaa8
HoangHuu <huuhoangcd221@gmail.com>
2018-06-06 14:59:48 +0700

    Revert template functions

--- inc/Frontend/hooks.php
+++ inc/Frontend/hooks.php
@@ -9,4 +9,30 @@ add_action( 'awebooking_display_search_result_item', 'awebooking_search_result_i
 add_action( 'awebooking_checkout_guest_details',     'awebooking_checkout_guest_details', 10 );
 add_action( 'awebooking_checkout_guest_details',     'awebooking_checkout_additionals', 20   );
 add_action( 'awebooking_checkout_payments',          'awebooking_checkout_payments', 10      );
+
+add_action( 'awebooking/before_main_content', 'awebooking_output_content_wrapper', 10 );
+add_action( 'awebooking/after_main_content', 'awebooking_output_content_wrapper_end', 10 );
+
+add_action( 'awebooking/template_notices', 'awebooking_template_notices', 10 );
+
+add_action( 'awebooking/sidebar', 'awebooking_get_sidebar', 10 );
+
+add_action( 'awebooking/before_archive_loop_item', 'awebooking_template_loop_room_type_link_open', 10 );
+add_action( 'awebooking/before_archive_loop_item_title', 'awebooking_template_loop_room_type_link_close', 20 );
+
+add_action( 'awebooking/after_archive_loop_item', 'awebooking_template_loop_view_more', 10 );
+add_action( 'awebooking/before_archive_loop_item_title', 'awebooking_template_loop_room_type_thumbnail', 10 );
+add_action( 'awebooking/archive_loop_item_title', 'awebooking_template_loop_room_type_title', 10 );
+
+add_action( 'awebooking/after_archive_loop_item_title', 'awebooking_template_loop_price', 10 );
+add_action( 'awebooking/after_archive_loop_item_title', 'awebooking_template_loop_description', 20 );
+
+
+add_action( 'awebooking/before_single_room_type_summary', 'awebooking_show_room_type_images', 20 );
+add_action( 'awebooking/room_type_thumbnails', 'awebooking_show_room_type_thumbnails', 10 );
+
+// add_action( 'awebooking/single_room_type_summary', 'awebooking_template_single_title', 5 );
+// add_action( 'awebooking/single_room_type_summary', 'awebooking_template_single_price', 10 );
+// add_action( 'awebooking/single_room_type_summary', 'awebooking_template_single_form', 15 );
+
 // @codingStandardsIgnoreEnd
