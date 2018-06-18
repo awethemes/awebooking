5c06bad5011efbb1083690a848815f76186c7015
Nguyễn Quốc Khánh <anhsemimcuoikhithayem@gmail.com>
2018-06-18 16:36:54 +0700

    Update code

--- templates/search/result-item.php
+++ templates/search/result-item.php
@@ -20,6 +20,244 @@ $rate_plan = $room_rate->get_rate_plan();
 
 ?>
 
+<div class="roommaster">
+	<div class="roommaster-header">
+		<h3 class="roommaster-header__title">
+			<a href="<?php echo esc_url( get_permalink( $room_type->get_id() ) ); ?>" rel="bookmark" target="_blank"><?php echo esc_html( $room_type->get( 'title' ) ); ?></a>
+		</h3>
+	</div>
+	<div class="roommaster-content awebooking">
+		<div class="columns no-gutters">
+			<div class="column-3">
+				<div class="roommaster-info">
+					<div class="roommaster-info__title">Loại phòng</div>
+					<div class="roommaster-info__image">
+						<a href="#">
+							<?php
+								if ( has_post_thumbnail( $room_type->get_id() ) ) {
+									echo get_the_post_thumbnail( $room_type->get_id(), 'awebooking_archive' );
+								}
+							?>
+							<img src="https://picsum.photos/200/200" alt="">
+							<span>Hình ảnh và chi tiết khác</span>
+						</a>
+					</div>
+					<ul class="roommaster-info__list">
+						<li class="info-item">
+							<span class="info-icon">
+								<i>K</i>
+							</span>
+							Hướng phòng: Đường phố
+						</li>
+						<li class="info-item">
+							<span class="info-icon">
+								<i>K</i>
+							</span>
+							35 m²/377 ft²
+						</li>
+						<li class="info-item">
+							<span class="info-icon">
+								<i>K</i>
+							</span>
+							vòi hoa sen
+						</li>
+						<li class="info-item">
+							<span class="info-icon">
+								<i>K</i>
+							</span>
+							3 giường đôi
+						</li>
+					</ul>
+				</div>
+			</div>
+			<div class="column-9">
+				<div class="roommaster-list">
+					<table class="roommaster-table">
+						<thead>
+							<tr>
+								<th class="thead-1">Lựa chọn</th>
+								<th class="thead-2">Sức chứa</th>
+								<th class="thead-3">Giá phòng/đêm</th>
+								<th class="thead-4">SL</th>
+								<th class="thead-5">Đặt nhiều nhất</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td class="roommaster-child">
+									<div class="roommaster-child__item">
+										<span class="roommaster-child__bucketspan">Lợi ích</span>
+										<div class="roommaster-child__info">
+											<i class="info-icon">K</i>
+											<span class="info-text">Chỉ thanh toán vào ngày 19 Tháng Sáu 2018</span>
+										</div>
+										<div class="roommaster-child__info">
+											<i class="info-icon">K</i>
+											<span>Chỉ thanh toán vào ngày 19 Tháng Sáu 2018</span>
+										</div>
+									</div>
+									<div class="roommaster-child__item">
+										<span class="roommaster-child__bucketspan">Giảm giá</span>
+										<div class="roommaster-child__info">
+											<i class="info-icon">K</i>
+											<span>Coupon giảm giá: 73.202 ₫</span>
+										</div>
+									</div>
+								</td>
+								<td class="roommaster-occupancy">
+									<span class="roommaster-occupancy__item">
+										<span>6</span>
+										x
+										<i>K</i>
+									</span>
+
+									<span class="roommaster-occupancy__item">
+										<span>3</span>
+										x
+										<i>K</i>
+									</span>
+								</td>
+								<td  class="roommaster-inventory">
+									<?php
+									switch ( abrs_get_option( 'display_price', 'total' ) ) {
+										case 'total':
+											abrs_price( $room_rate->get_rate() );
+											echo sprintf( 'Cost for %s nights', $room_rate->timespan->nights() );
+											break;
+
+										case 'first_night':
+											break;
+									}
+									?>
+								</td>
+								<td class="roommaster-select">
+									<select name="" id="" class="select-form">
+										<option value="1">1</option>
+										<option value="2">2</option>
+										<option value="3">3</option>
+										<option value="4">4</option>
+									</select>
+								</td>
+								<td class="roommaster-button">
+									<?php if ( ! $room_rate->has_error() ) : ?>
+										<?php
+										abrs_bookroom_button([
+											'room_type'   => $room_type->get_id(),
+											'show_button' => true,
+											'button_atts' => [
+												'class' => 'booknow button is-primary',
+											],
+										]);
+										?>
+									<?php endif ?>
+
+									<span class="roommaster-button__remaining-rooms">
+										<?php
+										$rooms_left = $remain_rooms->count();
+
+										if ( $rooms_left <= 2 ) {
+											/* translators: %s Number of remain rooms */
+											printf( esc_html( _nx( 'Only %s room left', 'Only %s rooms left', $rooms_left, 'remain rooms', 'awebooking' ) ), esc_html( number_format_i18n( $rooms_left ) ) );
+										} else {
+											/* translators: %s Number of remain rooms */
+											printf( esc_html_x( '%s rooms left', 'remain rooms', 'awebooking' ), esc_html( number_format_i18n( $rooms_left ) ) );
+										}
+										?>
+									</span>
+								</td>
+							</tr>
+							<tr>
+								<td class="roommaster-child">
+									<div class="roommaster-child__item">
+										<span class="roommaster-child__bucketspan">Lợi ích</span>
+										<div class="roommaster-child__info">
+											<i class="info-icon">K</i>
+											<span class="info-text">Chỉ thanh toán vào ngày 19 Tháng Sáu 2018</span>
+										</div>
+										<div class="roommaster-child__info">
+											<i class="info-icon">K</i>
+											<span>Chỉ thanh toán vào ngày 19 Tháng Sáu 2018</span>
+										</div>
+									</div>
+									<div class="roommaster-child__item">
+										<span class="roommaster-child__bucketspan">Giảm giá</span>
+										<div class="roommaster-child__info">
+											<i class="info-icon">K</i>
+											<span>Coupon giảm giá: 73.202 ₫</span>
+										</div>
+									</div>
+								</td>
+								<td class="roommaster-occupancy">
+									<span class="roommaster-occupancy__item">
+										<span>6</span>
+										x
+										<i>K</i>
+									</span>
+
+									<span class="roommaster-occupancy__item">
+										<span>3</span>
+										x
+										<i>K</i>
+									</span>
+								</td>
+								<td  class="roommaster-inventory">
+									<?php
+									switch ( abrs_get_option( 'display_price', 'total' ) ) {
+										case 'total':
+											abrs_price( $room_rate->get_rate() );
+											echo sprintf( 'Cost for %s nights', $room_rate->timespan->nights() );
+											break;
+
+										case 'first_night':
+											break;
+									}
+									?>
+								</td>
+								<td class="roommaster-select">
+									<select name="" id="" class="select-form">
+										<option value="1">1</option>
+										<option value="2">2</option>
+										<option value="3">3</option>
+										<option value="4">4</option>
+									</select>
+								</td>
+								<td class="roommaster-button">
+									<?php if ( ! $room_rate->has_error() ) : ?>
+										<?php
+										abrs_bookroom_button([
+											'room_type'   => $room_type->get_id(),
+											'show_button' => true,
+											'button_atts' => [
+												'class' => 'booknow button is-primary',
+											],
+										]);
+										?>
+									<?php endif ?>
+
+									<span class="abroom__remaining-rooms">
+										<?php
+										$rooms_left = $remain_rooms->count();
+
+										if ( $rooms_left <= 2 ) {
+											/* translators: %s Number of remain rooms */
+											printf( esc_html( _nx( 'Only %s room left', 'Only %s rooms left', $rooms_left, 'remain rooms', 'awebooking' ) ), esc_html( number_format_i18n( $rooms_left ) ) );
+										} else {
+											/* translators: %s Number of remain rooms */
+											printf( esc_html_x( '%s rooms left', 'remain rooms', 'awebooking' ), esc_html( number_format_i18n( $rooms_left ) ) );
+										}
+										?>
+									</span>
+								</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+
 <div class="box abroom">
 	<div class="abroom__data">
 		<table class="abroom__table">
