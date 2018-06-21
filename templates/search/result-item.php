0622c826f44165143a9a3738f6b3bee3273fd15e
HoangHuu <huuhoangcd221@gmail.com>
2018-06-21 16:11:35 +0700

    Add short description + amenities tab.

--- templates/search/result-item.php
+++ templates/search/result-item.php
@@ -33,9 +33,9 @@ $rate_plan = $room_rate->get_rate_plan();
 						<div class="roommaster-info__title"><?php esc_html_e( 'Room type', 'awebooking' ); ?></div>
 						<div class="roommaster-info__image">
 							<?php
-								if ( has_post_thumbnail( $room_type->get_id() ) ) {
-									echo get_the_post_thumbnail( $room_type->get_id(), 'awebooking_archive' );
-								}
+							if ( has_post_thumbnail( $room_type->get_id() ) ) {
+								echo get_the_post_thumbnail( $room_type->get_id(), 'awebooking_archive' );
+							}
 							?>
 						</div>
 
@@ -236,38 +236,47 @@ $rate_plan = $room_rate->get_rate_plan();
 		<div class="roommaster-detail">
 			<div class="columns">
 				<div class="column-3">
-					<img src="https://picsum.photos/500/500" alt="">
+					<?php
+					if ( has_post_thumbnail( $room_type->get_id() ) ) {
+						echo get_the_post_thumbnail( $room_type->get_id(), 'awebooking_archive' );
+					}
+					?>
 				</div>
 
 				<div class="column-9">
 					<div class="roommaster-tab tabs-main">
 						<ul class="roommaster-tab__list tabs-main-list">
-							<li class="active" rel="tab1">
-								Tab 1
+							<li class="active" rel="short-description-<?php echo absint( $room_type->get_id() ); ?>">
+								<?php esc_html_e( 'Short description', 'awebooking' ); ?>
 							</li>
-							<li rel="tab2">
-								Tab 2
-							</li>
-							<li rel="tab3">
-								Tab 3
-							</li>
-							<li rel="tab4">
-								Tab 4
+							<li rel="amenities-<?php echo absint( $room_type->get_id() ); ?>">
+								<?php esc_html_e( 'Amenities', 'awebooking' ); ?>
 							</li>
 							<div class="tabs-active-divider"></div>
 						</ul>
+
 						<div class="roommaster-tab__container tabs-main-container">
-							<div id="tab1" class="tabs-main-content">
-								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam doloribus autem modi praesentium blanditiis provident molestiae cumque possimus, enim quas, incidunt fuga a inventore facere. Expedita molestias quidem nisi cum?</p>
+							<div id="short-description-<?php echo absint( $room_type->get_id() ); ?>" class="tabs-main-content">
+								<?php echo esc_html( $room_type->get( 'short_description' ) ); ?>
 							</div>
-							<div id="tab2" class="tabs-main-content">
-								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam doloribus autem modi praesentium blanditiis provident molestiae cumque possimus, enim quas, incidunt fuga a inventore facere. Expedita molestias quidem nisi cum?</p>
-							</div>
-							<div id="tab3" class="tabs-main-content">
-								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam doloribus autem modi praesentium blanditiis provident molestiae cumque possimus, enim quas, incidunt fuga a inventore facere. Expedita molestias quidem nisi cum?</p>
-							</div>
-							<div id="tab4" class="tabs-main-content">
-								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam doloribus autem modi praesentium blanditiis provident molestiae cumque possimus, enim quas, incidunt fuga a inventore facere. Expedita molestias quidem nisi cum?</p>
+							<div id="amenities-<?php echo absint( $room_type->get_id() ); ?>" class="tabs-main-content">
+								<?php $amenities = wp_get_post_terms( $room_type->get_id(), 'hotel_amenity' ); ?>
+								<?php foreach ( $amenities as $amenity ) : ?>
+									<div class="room-amenity">
+										<?php if ( $icon = get_term_meta( $amenity->term_id, '_icon', true ) ) : ?>
+											<span class="room-amenity__icon">
+												<?php if ( 'svg' === $icon['type'] || 'image' === $icon['type'] ) : ?>
+													<?php echo wp_get_attachment_image( $icon['icon'] ); ?>
+												<?php else : ?>
+													<i class="<?php echo esc_attr( $icon['type'] . ' ' . $icon['icon'] ); ?>"></i>
+												<?php endif; ?>
+											</span>
+										<?php else : ?>
+											<i class="aficon aficon-checkmark"></i>
+										<?php endif; ?>
+										<span class="room-amenity__title"><?php echo esc_html( $amenity->name ); ?></span>
+									</div>
+								<?php endforeach; ?>
 							</div>
 						</div>
 					</div>
