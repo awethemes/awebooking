76e1ba20cd2f987f10568ab5117e01b391723f8b
Nguyễn Quốc Khánh <anhsemimcuoikhithayem@gmail.com>
2018-06-22 15:48:23 +0700

    Update code

--- templates/reservation/booked-multiple.php
+++ templates/reservation/booked-multiple.php
@@ -21,79 +21,81 @@ $reservation = abrs_reservation();
 ?>
 
 <?php foreach ( $room_stays as $key => $room_stay ) : ?>
-	<?php
-		$room_type = $room_stay->model();
-		$res_request = $room_stay->data->request;
-	?>
 
-	<a href="<?php echo esc_url( abrs_route( "/reservation/remove/{$room_stay->get_row_id()}" ) ); ?>">
-		<span><?php echo esc_html__( 'Remove', 'awebooking' ) ?></span>
-	</a>
-
-	<dl class="roomdetails-room">
-		<dt class="roomdetails-room__title"><?php esc_html_e( 'Room', 'awebooking' ); ?></dt>
-		<dd class="roomdetails-room__text"><?php echo esc_html( $room_stay->get( 'name' ) ); ?></dd>
-
-		<dt class="roomdetails-room__title"><?php esc_html_e( 'Stay', 'awebooking' ); ?></dt>
-		<dd class="roomdetails-room__text" class="occupancy-details">
+	<div class="roomdetails-room">
+		<div class="roomdetails-room__content">
 			<?php
-				/* translators: %1$s nights, %2$s guest */
-				printf( esc_html_x( '%1$s, %2$s', 'room stay', 'awebooking' ),
-					abrs_format_night_counts( $res_request['nights'] ),
-					abrs_format_guest_counts( $res_request->get_guest_counts() )
-				); // WPCS: xss ok.
+				$room_type = $room_stay->model();
+				$res_request = $room_stay->data->request;
 			?>
-		</dd>
 
-		<dt class="roomdetails-room__title"><?php esc_html_e( 'Max occupancy', 'awebooking' ); ?></dt>
-		<dd class="roomdetails-room__text">
-			<?php
-				/* translators: %s max occupancy */
-				printf( esc_html_x( '%s people', 'max occupancy', 'awebooking' ), absint( $room_type->get( 'maximum_occupancy' ) ) );
-			?>
-		</dd>
-	</dl>
+			<a href="<?php echo esc_url( abrs_route( "/reservation/remove/{$room_stay->get_row_id()}" ) ); ?>">
+				<span><?php echo esc_html__( 'Remove', 'awebooking' ) ?></span>
+			</a>
 
-	<div class="roomdetails-price">
-		<dl class="roomdetails-price-base">
-			<dt>
-				<?php
-					/* translators: %1$s quantity, %2$s nights */
-					printf( esc_html__( 'Price (%1$s x %2$s)', 'awebooking' ),
-						sprintf(
-							'%1$d %2$s',
-							esc_html( $room_stay->get( 'quantity' ) ),
-							esc_html( _n( 'room', 'rooms', $room_stay->get( 'quantity' ), 'awebooking' ) )
-						),
-						abrs_format_night_counts( $res_request['nights'] )
-					); // WPCS: xss ok.
-				?>
-			</dt>
-			<dd><?php echo abrs_price( $room_stay->get_total_price_exc_tax() ); // WPCS: xss ok. ?></dd>
+			<dl class="roomdetails-room__list">
+				<dt class="roomdetails-room__title"><?php esc_html_e( 'Room', 'awebooking' ); ?></dt>
+				<dd class="roomdetails-room__text"><?php echo esc_html( $room_stay->get( 'name' ) ); ?></dd>
 
-			<dt>VAT</dt>
-			<dd class="roomdetails-price-base__text roomdetails-tax">free</dd>
+				<dt class="roomdetails-room__title"><?php esc_html_e( 'Stay', 'awebooking' ); ?></dt>
+				<dd class="roomdetails-room__text" class="occupancy-details">
+					<?php
+						/* translators: %1$s nights, %2$s guest */
+						printf( esc_html_x( '%1$s, %2$s', 'room stay', 'awebooking' ),
+							abrs_format_night_counts( $res_request['nights'] ),
+							abrs_format_guest_counts( $res_request->get_guest_counts() )
+						); // WPCS: xss ok.
+					?>
+				</dd>
 
-			<dt>Subtotal</dt>
-			<dd class="roomdetails-price-base__text roomdetails-subtotal">
-				<?php echo abrs_price( $room_stay->get_total_price() ); // WPCS: xss ok. ?>
-			</dd>
-		</dl>
+				<dt class="roomdetails-room__title"><?php esc_html_e( 'Max occupancy', 'awebooking' ); ?></dt>
+				<dd class="roomdetails-room__text">
+					<?php
+						/* translators: %s max occupancy */
+						printf( esc_html_x( '%s people', 'max occupancy', 'awebooking' ), absint( $room_type->get( 'maximum_occupancy' ) ) );
+					?>
+				</dd>
+			</dl>
+		</div>
+
+		<div class="roomdetails-price">
+			<dl>
+				<dt>
+					<?php
+						/* translators: %1$s quantity, %2$s nights */
+						printf( esc_html__( 'Price (%1$s x %2$s)', 'awebooking' ),
+							sprintf(
+								'%1$d %2$s',
+								esc_html( $room_stay->get( 'quantity' ) ),
+								esc_html( _n( 'room', 'rooms', $room_stay->get( 'quantity' ), 'awebooking' ) )
+							),
+							abrs_format_night_counts( $res_request['nights'] )
+						); // WPCS: xss ok.
+					?>
+				</dt>
+				<dd><?php echo abrs_price( $room_stay->get_total_price_exc_tax() ); // WPCS: xss ok. ?></dd>
+
+				<dt>VAT</dt>
+				<dd class="roomdetails-price__vat">free</dd>
+
+				<dt>Subtotal</dt>
+				<dd>
+					<?php echo abrs_price( $room_stay->get_total_price() ); // WPCS: xss ok. ?>
+				</dd>
+			</dl>
+		</div>
 	</div>
 
 <?php endforeach; ?>
 
-<div class="roomdetails-price">
+<div class="roomdetails-price roomdetails-total">
+	<dl>
+		<dt><?php esc_html_e( 'Total', 'awebooking' ); ?></dt>
+		<dd><?php abrs_price( $reservation->get_total() ) ?></dd>
+	</dl>
 
-	<div class="roomdetails-price-footer">
-		<dl class="roomdetails-price-total">
-			<dt><?php esc_html_e( 'Total', 'awebooking' ); ?></dt>
-			<dd><?php abrs_price( $reservation->get_total() ) ?></dd>
-		</dl>
-
-		<p class="roomdetails-price-footer__info">
-			<strong>Giá đã bao gồm:</strong>
-			Phí dịch vụ 5%, Thuế 10%
-		</p>
-	</div>
+	<p class="roomdetails-total__info">
+		<strong>Giá đã bao gồm:</strong>
+		Phí dịch vụ 5%, Thuế 10%
+	</p>
 </div>
--- templates/reservation/booked-single.php
+++ templates/reservation/booked-single.php
@@ -20,50 +20,51 @@ $room_type = $room_stay->model();
 $res_request = $room_stay->data->request;
 ?>
 
-<dl class="roomdetails-room">
-	<dt class="roomdetails-room__title"><?php esc_html_e( 'Room', 'awebooking' ); ?></dt>
-	<dd class="roomdetails-room__text"><?php echo esc_html( $room_stay->get( 'name' ) ); ?></dd>
+<div class="roomdetails-room">
+	<dl class="roomdetails-room__list">
+		<dt class="roomdetails-room__title"><?php esc_html_e( 'Room', 'awebooking' ); ?></dt>
+		<dd class="roomdetails-room__text"><?php echo esc_html( $room_stay->get( 'name' ) ); ?></dd>
 
-	<dt class="roomdetails-room__title"><?php esc_html_e( 'Stay', 'awebooking' ); ?></dt>
-	<dd class="roomdetails-room__text" class="occupancy-details">
-		<?php
-			/* translators: %1$s nights, %2$s guest */
-			printf( esc_html_x( '%1$s, %2$s', 'room stay', 'awebooking' ),
-				abrs_format_night_counts( $res_request['nights'] ),
-				abrs_format_guest_counts( $res_request->get_guest_counts() )
-			); // WPCS: xss ok.
-		?>
-	</dd>
+		<dt class="roomdetails-room__title"><?php esc_html_e( 'Stay', 'awebooking' ); ?></dt>
+		<dd class="roomdetails-room__text" class="occupancy-details">
+			<?php
+				/* translators: %1$s nights, %2$s guest */
+				printf( esc_html_x( '%1$s, %2$s', 'room stay', 'awebooking' ),
+					abrs_format_night_counts( $res_request['nights'] ),
+					abrs_format_guest_counts( $res_request->get_guest_counts() )
+				); // WPCS: xss ok.
+			?>
+		</dd>
 
-	<dt class="roomdetails-room__title"><?php esc_html_e( 'Max occupancy', 'awebooking' ); ?></dt>
-	<dd class="roomdetails-room__text">
-		<?php
-			/* translators: %s max occupancy */
-			printf( esc_html_x( '%s people', 'max occupancy', 'awebooking' ), absint( $room_type->get( 'maximum_occupancy' ) ) );
-		?>
-	</dd>
-</dl>
-
-<div class="roomdetails-price">
-	<dl class="roomdetails-price-base">
-		<dt>Price (1 room x 1 night)</dt>
-		<dd>700.814</dd>
-
-		<dt>VAT</dt>
-		<dd class="roomdetails-price-base__text roomdetails-price-tax">free</dd>
+		<dt class="roomdetails-room__title"><?php esc_html_e( 'Max occupancy', 'awebooking' ); ?></dt>
+		<dd class="roomdetails-room__text">
+			<?php
+				/* translators: %s max occupancy */
+				printf( esc_html_x( '%s people', 'max occupancy', 'awebooking' ), absint( $room_type->get( 'maximum_occupancy' ) ) );
+			?>
+		</dd>
 	</dl>
+
+	<div class="roomdetails-price">
+		<dl>
+			<dt>Price (1 room x 1 night)</dt>
+			<dd>700.814</dd>
+
+			<dt>VAT</dt>
+			<dd>free</dd>
+		</dl>
+	</div>
 </div>
 
-<div class="roomdetails-price">
-	<div class="roomdetails-price-footer">
-		<dl class="roomdetails-price-total">
-			<dt>Price</dt>
-			<dd>1.700.814</dd>
-		</dl>
 
-		<p class="roomdetails-price-footer__info">
-			<strong>Giá đã bao gồm:</strong>
-			Phí dịch vụ 5%, Thuế 10%
-		</p>
-	</div>
-<div>
+<div class="roomdetails-price roomdetails-total">
+	<dl>
+		<dt>Price</dt>
+		<dd>1.700.814</dd>
+	</dl>
+
+	<p class="roomdetails-total__info">
+		<strong>Giá đã bao gồm:</strong>
+		Phí dịch vụ 5%, Thuế 10%
+	</p>
+</div>
