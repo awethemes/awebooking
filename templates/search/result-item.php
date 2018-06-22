4a4ea603554aa5cdbff211be30953ffefdb25768
HoangHuu <huuhoangcd221@gmail.com>
2018-06-22 17:30:47 +0700

    Add occupancy tooltip

--- templates/search/result-item.php
+++ templates/search/result-item.php
@@ -141,7 +141,7 @@ $rate_plan = $room_rate->get_rate_plan();
 								</div>
 							</div>
 							<div class="column-3">
-								<div class="roommaster-occupancy">
+								<div class="roommaster-occupancy" data-awebooking="tooltip" data-tippy-html="#occupancy-description-<?php echo absint( $room_type->get_id() ); ?>" title="A">
 									<?php if ( $room_type->get( 'number_adults' ) ) : ?>
 										<span class="roommaster-occupancy__item">
 											<?php
@@ -179,12 +179,41 @@ $rate_plan = $room_rate->get_rate_plan();
 										</span>
 									<?php endif; ?>
 
-									<div>
-										(Tool tip)
-										Maximum occupancy: 4
-										Adults: 2
-										Children: 2
-									</div>
+								</div>
+								<div id="occupancy-description-<?php echo absint( $room_type->get_id() ); ?>" class="occupancy-description" style="display: none;">
+									<h4>
+										<?php
+											/* translators: %s maximum occupancy */
+											printf( esc_html__( 'Maximum occupancy: %s', 'awebooking' ), absint( $room_type->get( 'maximum_occupancy' ) ) );
+										?>
+									</h4>
+									<ul>
+										<li>
+											<?php
+												/* translators: %s number adults */
+												printf( esc_html__( 'Number adults: %s', 'awebooking' ), absint( $room_type->get( 'number_adults' ) ) );
+											?>
+										</li>
+
+										<?php if ( $room_type->get( 'number_children' ) ) : ?>
+											<li>
+												<?php
+													/* translators: %s number children */
+													printf( esc_html__( 'Number children: %s', 'awebooking' ), absint( $room_type->get( 'number_children' ) ) );
+												?>
+											</li>
+										<?php endif; ?>
+
+										<?php if ( $room_type->get( 'number_infants' ) ) : ?>
+											<li>
+												<?php
+													/* translators: %s number infants */
+													printf( esc_html__( 'Number infants: %s', 'awebooking' ), absint( $room_type->get( 'number_infants' ) ) );
+												?>
+											</li>
+										<?php endif; ?>
+									</ul>
+
 								</div>
 							</div>
 							<div class="column-3">
@@ -244,7 +273,7 @@ $rate_plan = $room_rate->get_rate_plan();
 						</div>
 						<?php //endforeach; ?>
 					</div>
-				</div>	
+				</div>
 			</div>
 		</div>
 
