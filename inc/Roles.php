a6a1728484fe512fb494b017a3ef99cf2d648939
Nguyen Van Anh <anhskohbo@gmail.com>
2018-07-02 16:46:24 +0700

    Code format

--- inc/Roles.php
+++ inc/Roles.php
@@ -19,41 +19,33 @@ class Roles {
 		}
 
 		// Hotel customer role: somebody who can only manage their profile, view and request some actions their booking.
-		add_role(
-			'awebooking_customer',
-			esc_html__( 'Hotel Customer', 'awebooking' ),
-			[
-				'read' => true,
-			]
-		);
+		add_role( 'awebooking_customer', esc_html__( 'Hotel Customer', 'awebooking' ), [
+			'read' => true,
+		] );
 
 		// Hotel receptionist: somebody who can view room types, bookings, services, amenities, manage to price,
 		// manage availability, assists guests making hotel reservations.
-		add_role(
-			'awebooking_receptionist',
-			esc_html__( 'Hotel Receptionist', 'awebooking' ),
-			[
-				'level_9'                => true,
-				'level_8'                => true,
-				'level_7'                => true,
-				'level_6'                => true,
-				'level_5'                => true,
-				'level_4'                => true,
-				'level_3'                => true,
-				'level_2'                => true,
-				'level_1'                => true,
-				'level_0'                => true,
-				'read'                   => true,
-				'read_private_pages'     => true,
-				'read_private_posts'     => true,
-				'edit_posts'             => true,
-				'edit_pages'             => true,
-				'publish_posts'          => true,
-				'publish_pages'          => true,
-				'manage_links'           => true,
-				'moderate_comments'      => true,
-			]
-		);
+		add_role( 'awebooking_receptionist', esc_html__( 'Hotel Receptionist', 'awebooking' ), [
+			'level_9'                => true,
+			'level_8'                => true,
+			'level_7'                => true,
+			'level_6'                => true,
+			'level_5'                => true,
+			'level_4'                => true,
+			'level_3'                => true,
+			'level_2'                => true,
+			'level_1'                => true,
+			'level_0'                => true,
+			'read'                   => true,
+			'read_private_pages'     => true,
+			'read_private_posts'     => true,
+			'edit_posts'             => true,
+			'edit_pages'             => true,
+			'publish_posts'          => true,
+			'publish_pages'          => true,
+			'manage_links'           => true,
+			'moderate_comments'      => true,
+		] );
 
 		foreach ( $this->get_core_receptionist_capabilities() as $cap_group ) {
 			foreach ( $cap_group as $cap ) {
@@ -62,51 +54,47 @@ class Roles {
 		}
 
 		// Hotel manager role: somebody who has access to all the AweBooking's features.
-		add_role(
-			'awebooking_manager',
-			esc_html__( 'Hotel Manager', 'awebooking' ),
-			[
-				'level_9'                => true,
-				'level_8'                => true,
-				'level_7'                => true,
-				'level_6'                => true,
-				'level_5'                => true,
-				'level_4'                => true,
-				'level_3'                => true,
-				'level_2'                => true,
-				'level_1'                => true,
-				'level_0'                => true,
-				'read'                   => true,
-				'read_private_pages'     => true,
-				'read_private_posts'     => true,
-				'edit_users'             => true,
-				'edit_posts'             => true,
-				'edit_pages'             => true,
-				'edit_published_posts'   => true,
-				'edit_published_pages'   => true,
-				'edit_private_pages'     => true,
-				'edit_private_posts'     => true,
-				'edit_others_posts'      => true,
-				'edit_others_pages'      => true,
-				'publish_posts'          => true,
-				'publish_pages'          => true,
-				'delete_posts'           => true,
-				'delete_pages'           => true,
-				'delete_private_pages'   => true,
-				'delete_private_posts'   => true,
-				'delete_published_pages' => true,
-				'delete_published_posts' => true,
-				'delete_others_posts'    => true,
-				'delete_others_pages'    => true,
-				'manage_categories'      => true,
-				'manage_links'           => true,
-				'moderate_comments'      => true,
-				'upload_files'           => true,
-				'export'                 => true,
-				'import'                 => true,
-				'list_users'             => true,
-			]
-		);
+		add_role( 'awebooking_manager', esc_html__( 'Hotel Manager', 'awebooking' ), [
+			'level_9'                => true,
+			'level_8'                => true,
+			'level_7'                => true,
+			'level_6'                => true,
+			'level_5'                => true,
+			'level_4'                => true,
+			'level_3'                => true,
+			'level_2'                => true,
+			'level_1'                => true,
+			'level_0'                => true,
+			'read'                   => true,
+			'read_private_pages'     => true,
+			'read_private_posts'     => true,
+			'edit_users'             => true,
+			'edit_posts'             => true,
+			'edit_pages'             => true,
+			'edit_published_posts'   => true,
+			'edit_published_pages'   => true,
+			'edit_private_pages'     => true,
+			'edit_private_posts'     => true,
+			'edit_others_posts'      => true,
+			'edit_others_pages'      => true,
+			'publish_posts'          => true,
+			'publish_pages'          => true,
+			'delete_posts'           => true,
+			'delete_pages'           => true,
+			'delete_private_pages'   => true,
+			'delete_private_posts'   => true,
+			'delete_published_pages' => true,
+			'delete_published_posts' => true,
+			'delete_others_posts'    => true,
+			'delete_others_pages'    => true,
+			'manage_categories'      => true,
+			'manage_links'           => true,
+			'moderate_comments'      => true,
+			'upload_files'           => true,
+			'export'                 => true,
+			'import'                 => true,
+			'list_users'             => true,
+		] );
 
 		foreach ( $this->get_core_manager_capabilities() as $cap_group ) {
 			foreach ( $cap_group as $cap ) {
