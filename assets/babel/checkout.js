752eb31a8bfacdc41b047415db79f6d96c40d844
Nguyen Van Anh <anhskohbo@gmail.com>
2018-08-02 16:58:13 +0700

    Checkout js

--- assets/babel/checkout.js
+++ assets/babel/checkout.js
@@ -1,7 +1,69 @@
 import $ from 'jquery';
 
-$('.payment-method').on('click', function () {
-  $('.payment-method').removeClass('selected')
-  $(this).addClass('selected')
-  $(this).find('input').prop('checked', true)
+const SELECTED = 'selected';
+
+const Selector = {
+  ROOT:     '.payment-methods',
+  ITEM:     '.payment-method',
+  INPUT:    'input[type="radio"]',
+  SELECTED: '.selected',
+}
+
+const _handleLabelClick = function(e) {
+  const element = e.currentTarget
+  const input   = element.querySelector(Selector.INPUT)
+
+  // Prevent action on non-input.
+  if (!input) {
+    return;
+  }
+
+  let triggerChange = true
+  const rootElement = $(element).closest(Selector.ROOT)[0]
+
+  if (input.checked && $(element).hasClass(SELECTED)) {
+    triggerChange = false
+  } else {
+    $(rootElement).children(Selector.SELECTED).removeClass(SELECTED)
+  }
+
+  if (triggerChange) {
+    if (input.hasAttribute('disabled') || input.classList.contains('disabled')) {
+      e.preventDefault()
+      return
+    }
+
+    input.checked = !element.classList.contains(SELECTED)
+
+    input.focus()
+    $(input).trigger('change')
+
+    $(element).addClass(SELECTED)
+  }
+}
+
+const _triggerPaymentMethod = function(e) {
+  const input = e.currentTarget
+  if (!input.checked) {
+    return
+  }
+
+  const root  = $(input).closest(Selector.ROOT)[0]
+  const event = $.Event('selected.awebooking.gateway', {
+    relatedTarget: input
+  })
+
+  $(root).trigger(event, input.value);
+}
+
+$(function () {
+  const $el = $('#payment-methods')
+    .on('click', Selector.ITEM, _handleLabelClick)
+    .on('change', Selector.INPUT, _triggerPaymentMethod);
+
+  setTimeout(() => {
+    $el.find(`${Selector.INPUT}:checked`)
+      .closest(Selector.ITEM)
+      .trigger('click')
+  }, 0)
 })
