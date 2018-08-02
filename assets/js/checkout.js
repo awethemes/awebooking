92a3d212ca5e57a18e3644f24d964e0f67e8eeed
Nguyen Van Anh <anhskohbo@gmail.com>
2018-08-02 17:22:05 +0700

    Build

--- assets/js/checkout.js
+++ assets/js/checkout.js
@@ -3,10 +3,68 @@
 
   $ = $ && $.hasOwnProperty('default') ? $['default'] : $;
 
-  $('.payment-method').on('click', function () {
-    $('.payment-method').removeClass('selected');
-    $(this).addClass('selected');
-    $(this).find('input').prop('checked', true);
+  var SELECTED = 'selected';
+
+  var Selector = {
+    ROOT: '.payment-methods',
+    ITEM: '.payment-method',
+    INPUT: 'input[type="radio"]',
+    SELECTED: '.selected'
+  };
+
+  var _handleLabelClick = function _handleLabelClick(e) {
+    var element = e.currentTarget;
+    var input = element.querySelector(Selector.INPUT);
+
+    // Prevent action on non-input.
+    if (!input) {
+      return;
+    }
+
+    var triggerChange = true;
+    var rootElement = $(element).closest(Selector.ROOT)[0];
+
+    if (input.checked && $(element).hasClass(SELECTED)) {
+      triggerChange = false;
+    } else {
+      $(rootElement).children(Selector.SELECTED).removeClass(SELECTED);
+    }
+
+    if (triggerChange) {
+      if (input.hasAttribute('disabled') || input.classList.contains('disabled')) {
+        e.preventDefault();
+        return;
+      }
+
+      input.checked = !element.classList.contains(SELECTED);
+
+      input.focus();
+      $(input).trigger('change');
+
+      $(element).addClass(SELECTED);
+    }
+  };
+
+  var _triggerPaymentMethod = function _triggerPaymentMethod(e) {
+    var input = e.currentTarget;
+    if (!input.checked) {
+      return;
+    }
+
+    var root = $(input).closest(Selector.ROOT)[0];
+    var event = $.Event('selected.awebooking.gateway', {
+      relatedTarget: input
+    });
+
+    $(root).trigger(event, input.value);
+  };
+
+  $(function () {
+    var $el = $('#payment-methods').on('click', Selector.ITEM, _handleLabelClick).on('change', Selector.INPUT, _triggerPaymentMethod);
+
+    setTimeout(function () {
+      $el.find(Selector.INPUT + ':checked').closest(Selector.ITEM).trigger('click');
+    }, 0);
   });
 
 }(jQuery));
