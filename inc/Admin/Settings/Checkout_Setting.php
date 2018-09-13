300b56c1c2e67499f5882212776b01cd1f3f9ae3
Nguyen Van Anh <anhskohbo@gmail.com>
2018-09-13 16:30:34 +0700

    Code format

--- inc/Admin/Settings/Checkout_Setting.php
+++ inc/Admin/Settings/Checkout_Setting.php
@@ -21,39 +21,40 @@ class Checkout_Setting extends Abstract_Setting {
 	 */
 	public function setup_fields() {
 		$options = $this->add_section( 'checkout-options', [
-			'title'      => esc_html__( 'Options', 'awebooking' ),
-			'priority'   => 0,
-		]);
+			'title'    => esc_html__( 'Options', 'awebooking' ),
+			'priority' => 0,
+		] );
 
-		$options->add_field([
-			'id'         => '__payments_title',
-			'type'       => 'title',
-			'name'       => esc_html__( 'Payment Gateways', 'awebooking' ),
-			'desc'       => abrs_esc_text( __( 'Installed gateways are listed below. You can find more payment gateways on <a href="https://awethemes.com/plugins/awebooking#premiumaddons">Premium addons</a>.', 'awebooking' ) ),
-		]);
+		$options->add_field( [
+			'id'   => '__payments_title',
+			'type' => 'title',
+			'name' => esc_html__( 'Payment Gateways', 'awebooking' ),
+			'desc' => abrs_esc_text( __( 'Installed gateways are listed below. You can find more payment gateways on <a href="https://awethemes.com/plugins/awebooking#premiumaddons">Premium addons</a>.',
+				'awebooking' ) ),
+		] );
 
-		$options->add_field([
-			'id'         => 'list_gateway_order',
-			'type'       => 'include',
-			'name'       => esc_html__( 'Gateway Display Order', 'awebooking' ),
-			'desc'       => esc_html__( 'Drag and drop to control display order on the frontend.', 'awebooking' ),
-			'tooltip'    => true,
-			'include'    => trailingslashit( dirname( __DIR__ ) ) . 'views/settings/html-gateways-sorter.php',
-		]);
+		$options->add_field( [
+			'id'      => 'list_gateway_order',
+			'type'    => 'include',
+			'name'    => esc_html__( 'Gateway Display Order', 'awebooking' ),
+			'desc'    => esc_html__( 'Drag and drop to control display order on the frontend.', 'awebooking' ),
+			'tooltip' => true,
+			'include' => trailingslashit( dirname( __DIR__ ) ) . 'views/settings/html-gateways-sorter.php',
+		] );
 
-		$options->add_field([
-			'id'         => '__checkout_title',
-			'type'       => 'title',
-			'name'       => esc_html__( 'Checkout', 'awebooking' ),
-		]);
+		$options->add_field( [
+			'id'   => '__checkout_title',
+			'type' => 'title',
+			'name' => esc_html__( 'Checkout', 'awebooking' ),
+		] );
 
-		$options->add_field([
+		$options->add_field( [
 			'id'              => 'list_checkout_controls',
 			'type'            => 'include',
 			'name'            => esc_html__( 'Checkout Controls', 'awebooking' ),
 			'include'         => trailingslashit( dirname( __DIR__ ) ) . 'views/settings/html-checkout-controls.php',
 			'sanitization_cb' => [ $this, 'sanitize_checkout_controls' ],
-		]);
+		] );
 
 		// Register the gateways custom fields.
 		foreach ( abrs_payment_gateways()->all() as $gateway ) {
@@ -77,24 +78,29 @@ class Checkout_Setting extends Abstract_Setting {
 			'title' => $gateway->get_method_title(),
 		]);
 
-		$section->add_field([
-			'id'    => '__title_' . $prefix,
-			'type'  => 'title',
-			'name'  => $gateway->get_method_title(),
-			'desc'  => $gateway->get_method_description(),
-		]);
+		// TODO: ...
+		if ( ! $gateway->is_enabled() ) {
+			$section->hidden = true;
+		}
+
+		$section->add_field( [
+			'id'   => '__title_' . $prefix,
+			'type' => 'title',
+			'name' => $gateway->get_method_title(),
+			'desc' => $gateway->get_method_description(),
+		] );
 
 		// Loop each settings field and doing register.
 		foreach ( $gateway->get_setting_fields() as $key => $args ) {
-			$field_args = array_merge( $args, [ 'id' => $prefix . '_' . $key ] );
+			$_args = array_merge( $args, [ 'id' => $prefix . '_' . $key ] );
 
-			if ( 'checkbox' === $field_args['type'] ) {
-				$field_args['type'] = 'abrs_checkbox';
-			} elseif ( 'toggle' === $field_args['type'] ) {
-				$field_args['type'] = 'abrs_toggle';
+			if ( 'checkbox' === $_args['type'] ) {
+				$_args['type'] = 'abrs_checkbox';
+			} elseif ( 'toggle' === $_args['type'] ) {
+				$_args['type'] = 'abrs_toggle';
 			}
 
-			$section->add_field( $field_args );
+			$section->add_field( $_args );
 		}
 
 		do_action( 'abrs_register_gateway_settings', $gateway, $section );
