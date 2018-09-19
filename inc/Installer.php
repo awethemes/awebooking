0b1e15b8bea89a5ef044a40489008b162812d35e
Nguyen Van Anh <anhskohbo@gmail.com>
2018-09-19 15:22:59 +0700

    Prevent create default hotel

--- inc/Installer.php
+++ inc/Installer.php
@@ -239,7 +239,7 @@ class Installer {
 		$this->create_tables();
 		$this->create_roles();
 		$this->create_cron_jobs();
-		$this->create_default_hotel();
+		// $this->create_default_hotel();
 		$this->update_version();
 
 		// CHeck for the update DB.
