2d8c7df478d8bf87bd1ee5927655ee91a3e206a5
Nguyen Van Anh <anhskohbo@gmail.com>
2018-09-26 15:01:50 +0700

    Correct way to load dev packages

--- loader.php
+++ loader.php
@@ -9,8 +9,18 @@
  * First, we need autoload via Composer to make everything works.
  */
 require trailingslashit( __DIR__ ) . 'vendor/autoload.php';
-require trailingslashit( __DIR__ ) . 'awethemes/relationships/vendor/autoload.php';
 require trailingslashit( __DIR__ ) . 'vendor/webdevstudios/cmb2/init.php';
+
+// For dev only, will be remove in the future when packages stable.
+$_dev_packages = [
+	__DIR__ . '/awethemes/relationships/vendor/autoload.php',
+];
+
+foreach ( $_dev_packages as $_package ) {
+	if ( file_exists( $_package ) ) {
+		require_once $_package;
+	}
+}
 
 // Require helpers & functions.
 require trailingslashit( __DIR__ ) . 'inc/Support/helpers.php';
