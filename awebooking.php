3095bfe285017f9ff17ae6912ae38b593f06fb6e
Nguyen Van Anh <anhskohbo@gmail.com>
2018-11-30 17:59:24 +0700

    bump

--- awebooking.php
+++ awebooking.php
@@ -7,7 +7,7 @@
  * Author URI:      https://awethemes.com
  * Text Domain:     awebooking
  * Domain Path:     /languages
- * Version:         3.1.17
+ * Version:         3.1.18-dev
  *
  * @package         AweBooking
  */
