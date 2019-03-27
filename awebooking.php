7cc4e615f67f343c25f08f9d060ceef1848de33b
Nguyen Van Anh <anhskohbo@gmail.com>
2019-03-27 15:21:04 +0700

    prepare 3.2.11

--- awebooking.php
+++ awebooking.php
@@ -7,7 +7,7 @@
  * Author URI:      https://awethemes.com
  * Text Domain:     awebooking
  * Domain Path:     /languages
- * Version:         3.2.11-dev
+ * Version:         3.2.11
  *
  * @package         AweBooking
  */
--- inc/Plugin.php
+++ inc/Plugin.php
@@ -24,7 +24,7 @@ final class Plugin extends Container {
 	 *
 	 * @var string
 	 */
-	const VERSION = '3.2.10';
+	const VERSION = '3.2.11';
 
 	/**
 	 * The plugin file path.
