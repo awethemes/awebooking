d19e098677d88ac4bf3cd114afe7622767ff765d
Nguyen Van Anh <anhskohbo@gmail.com>
2019-05-10 18:07:21 +0700

    Fixed no search results

--- templates/search/results.php
+++ templates/search/results.php
@@ -41,19 +41,23 @@ $res_request = $results->get_request();
 				do_action( 'abrs_display_search_result_item', $res_request, $room_type, $room_rate, $availabilities );
 			}
 
-			foreach ( $results->get_invalid_items() as $availabilities ) {
-				list( $room_type, $room_rate ) = array_values( $availabilities );
+			if ( has_action( 'abrs_display_search_result_unavailable_item' ) ) {
+				foreach ( $results->get_invalid_items() as $availabilities ) {
+					list( $room_type, $room_rate ) = array_values( $availabilities );
 
-				/**
-				 * Fire action to display search result unavailable item.
-				 *
-				 * @hooked awebooking_search_result_item()
-				 *
-				 * @param \AweBooking\Availability\Request   $res_request    The current reservation request.
-				 * @param \AweBooking\Model\Room_Type        $room_type      The room type instance.
-				 * @param \AweBooking\Availability\Room_Rate $room_rate      The room rate instance.
-				 */
-				do_action( 'abrs_display_search_result_unavailable_item', $res_request, $room_type, $room_rate );
+					/**
+					 * Fire action to display search result unavailable item.
+					 *
+					 * @hooked awebooking_search_result_item()
+					 *
+					 * @param \AweBooking\Availability\Request   $res_request    The current reservation request.
+					 * @param \AweBooking\Model\Room_Type        $room_type      The room type instance.
+					 * @param \AweBooking\Availability\Room_Rate $room_rate      The room rate instance.
+					 */
+					do_action( 'abrs_display_search_result_unavailable_item', $res_request, $room_type, $room_rate );
+				}
+			} else {
+				abrs_get_template( 'search/no-results.php' );
 			}
 			?>
 		</div><!-- /.rooms -->
