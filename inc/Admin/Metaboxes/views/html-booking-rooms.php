560175f2f3b480819051fcb00ba70ceb87662c92
Nguyen Van Anh <anhskohbo@gmail.com>
2019-09-10 15:17:59 +0700

    WIP: Improve booking pricing

--- inc/Admin/Metaboxes/views/html-booking-rooms.php
+++ inc/Admin/Metaboxes/views/html-booking-rooms.php
@@ -3,7 +3,7 @@
 /* @var $the_booking \AweBooking\Model\Booking */
 global $the_booking;
 
-$columns = 9;
+$columns = 10;
 if ( ! abrs_children_bookable() ) {
 	$columns--;
 }
@@ -23,28 +23,53 @@ $room_items = $the_booking->get_rooms();
 		padding: 0;
 		border: none !important;
 	}
+
+	.price-area {
+		background: #f9f9f9;
+	}
+
+	th.price-area {
+		color: #696969 !important;
+		font-size: 10px !important;
+		font-weight: 500;
+		text-transform: uppercase;
+	}
+
+	td.price-area {
+		color: #111 !important;
+	}
 </style>
 
 <table class="awebooking-table abrs-booking-rooms widefat fixed">
 	<thead>
-	<tr>
-		<th style="width: 20%;"><?php echo esc_html__( 'Room Type', 'awebooking' ); ?></th>
-		<th style="width: 15%;"><?php echo esc_html__( 'Rate Plan', 'awebooking' ); ?></th>
-		<th style="width: 5%;"><span class="aficon aficon-moon tippy" title="<?php echo esc_html__( 'Nights', 'awebooking' ); ?>"></span><span class="screen-reader-text"><?php echo esc_html__( 'Nights', 'awebooking' ); ?></span></th>
-		<th style="width: 10%;"><?php echo esc_html__( 'Check In', 'awebooking' ); ?></th>
-		<th style="width: 10%;"><?php echo esc_html__( 'Check Out', 'awebooking' ); ?></th>
-		<th style="width: 7%;"><?php echo esc_html__( 'Adults', 'awebooking' ); ?></th>
+		<tr>
+			<th><?php echo esc_html__( 'Room Type', 'awebooking' ); ?></th>
+			<th><?php echo esc_html__( 'Rate Plan', 'awebooking' ); ?></th>
+			<th style="width: 10%;"><?php echo esc_html__( 'Check In', 'awebooking' ); ?></th>
+			<th style="width: 10%;"><?php echo esc_html__( 'Check Out', 'awebooking' ); ?></th>
+			<th style="width: 7%;"><?php echo esc_html__( 'Adults', 'awebooking' ); ?></th>
 
-		<?php if ( abrs_children_bookable() ) : ?>
-			<th style="width: 7%;"><?php echo esc_html__( 'Children', 'awebooking' ); ?></th>
-		<?php endif ?>
+			<?php if ( abrs_children_bookable() ) : ?>
+				<th style="width: 7%;"><?php echo esc_html__( 'Children', 'awebooking' ); ?></th>
+			<?php endif ?>
 
-		<?php if ( abrs_infants_bookable() ) : ?>
-			<th style="width: 7%;"><?php echo esc_html__( 'Infants', 'awebooking' ); ?></th>
-		<?php endif ?>
+			<?php if ( abrs_infants_bookable() ) : ?>
+				<th style="width: 7%;"><?php echo esc_html__( 'Infants', 'awebooking' ); ?></th>
+			<?php endif ?>
 
-		<th class="abrs-text-right"><span><?php esc_html_e( 'Price', 'awebooking' ); ?></span></th>
-	</tr>
+			<th class="abrs-text-right price-area" style="width: 80px;">
+				<span><?php esc_html_e( 'Unit Price', 'awebooking' ); ?></span>
+			</th>
+
+			<th class="abrs-text-right price-area" style="width: 60px;">
+				<span class="aficon aficon-moon tippy" style="font-size: 13px;" title="<?php echo esc_html__( 'Nights', 'awebooking' ); ?>"></span>
+				<span class="screen-reader-text"><?php echo esc_html__( 'Nights', 'awebooking' ); ?></span>
+			</th>
+
+			<th class="abrs-text-right price-area" style="width: 80px;">
+				<span><?php esc_html_e( 'Total', 'awebooking' ); ?></span>
+			</th>
+		</tr>
 	</thead>
 
 	<tbody>
@@ -77,13 +102,21 @@ $room_items = $the_booking->get_rooms();
 						$thumbnail = get_the_post_thumbnail( $room_type->get_id(), 'thumbnail' );
 					}
 
-					printf( '<div class="abrs-thumb-image abrs-fleft" style="margin-right: 10px;">%2$s</div>', esc_url( '#' ), $thumbnail ); // @wpcs: XSS OK.
+					printf( '<div class="abrs-thumb-image abrs-fleft" style="margin-right: 10px;">%1$s</div>', $thumbnail ); // @wpcs: XSS OK.
 					?>
 
 					<div>
 						<strong class="row-title"><?php echo esc_html( $room ? $room->get( 'name' ) : $item->get_name() ); ?></strong>
 						<span class="dp-block"><?php echo esc_html( $room_type ? $room_type->get( 'title' ) : '' ); ?></span>
 					</div>
+
+					<?php if ( $the_booking->is_editable() ) : ?>
+						<div class="row-actions abrs-fleft">
+							<span class="edit"><a href="<?php echo esc_url( $action_link ); ?>"><?php esc_html_e( 'Edit', 'awebooking' ); ?></a> | </span>
+							<span class="swap"><a href="<?php echo esc_url( add_query_arg( 'action', 'swap', $action_link ) ); ?>"><?php esc_html_e( 'Swap', 'awebooking' ); ?></a> | </span>
+							<span class="trash"><a href="<?php echo esc_url( wp_nonce_url( $action_link, "delete_room_{$item->get_id()}" ) ); ?>" data-method="abrs-delete"><?php esc_html_e( 'Delete', 'awebooking' ); ?></a></span>
+						</div>
+					<?php endif; ?>
 				</td>
 
 				<td>
@@ -91,17 +124,13 @@ $room_items = $the_booking->get_rooms();
 				</td>
 
 				<td>
-					<span class="abrs-badge"><?php echo esc_html( abrs_optional( $timespan )->get_nights() ); ?></span>
-				</td>
-
-				<td>
-					<?php if ( ! is_null( $timespan ) ) : ?>
+					<?php if ($timespan !== null) : ?>
 						<?php echo esc_html( abrs_format_date( $timespan->get_start_date() ) ); ?>
 					<?php endif ?>
 				</td>
 
 				<td>
-					<?php if ( ! is_null( $timespan ) ) : ?>
+					<?php if ($timespan !== null) : ?>
 						<?php echo esc_html( abrs_format_date( $timespan->get_end_date() ) ); ?>
 					<?php endif ?>
 				</td>
@@ -122,35 +151,17 @@ $room_items = $the_booking->get_rooms();
 					</td>
 				<?php endif ?>
 
-				<td class="abrs-text-right">
-					<?php if ( $the_booking->is_editable() ) : ?>
+				<td class="abrs-text-right price-area">
+					<?php abrs_price( $item->get( 'subtotal' ), $the_booking->get( 'currency' ) ); ?>
+					<?php abrs_price( $item->get( 'total_tax' ), $the_booking->get( 'currency' ) ); ?>
+				</td>
 
-						<div class="row-actions abrs-fleft">
-							<span class="edit"><a href="<?php echo esc_url( $action_link ); ?>"><?php esc_html_e( 'Edit', 'awebooking' ); ?></a> | </span>
-							<span class="swap"><a href="<?php echo esc_url( add_query_arg( 'action', 'swap', $action_link ) ); ?>"><?php esc_html_e( 'Swap', 'awebooking' ); ?></a> | </span>
-							<span class="trash"><a href="<?php echo esc_url( wp_nonce_url( $action_link, "delete_room_{$item->get_id()}" ) ); ?>" data-method="abrs-delete"><?php esc_html_e( 'Delete', 'awebooking' ); ?></a></span>
-						</div>
+				<td class="abrs-text-right price-area">
+					<span class="abrs-badge"><?php echo esc_html( abrs_optional( $timespan )->get_nights() ); ?></span>
+				</td>
 
-					<?php endif; ?>
-
-					<table class="abrs-fright abrs-inline-table">
-						<tbody>
-							<tr>
-								<th><?php esc_html_e( 'Subtotal:', 'awebooking' ); ?></th>
-								<td><?php abrs_price( $item->get( 'subtotal' ), $the_booking->get( 'currency' ) ); ?></td>
-							</tr>
-
-							<tr>
-								<th><?php esc_html_e( 'TAX:', 'awebooking' ); ?></th>
-								<td><?php abrs_price( $item->get( 'total_tax' ), $the_booking->get( 'currency' ) ); ?></td>
-							</tr>
-
-							<tr>
-								<th><?php esc_html_e( 'Total:', 'awebooking' ); ?></th>
-								<td><strong><?php abrs_price( $item->get( 'total' ), $the_booking->get( 'currency' ) ); ?></strong></td>
-							</tr>
-						</tbody>
-					</table>
+				<td class="abrs-text-right price-area">
+					<strong><?php abrs_price( $item->get( 'total' ), $the_booking->get( 'currency' ) ); ?></strong>
 				</td>
 			</tr>
 
