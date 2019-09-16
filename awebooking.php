230afe5e8b6113479d9b07e3cb7792442fa1d15a
Nguyen Van Anh <anhskohbo@gmail.com>
2019-09-16 15:44:35 +0700

    build new version

--- awebooking.php
+++ awebooking.php
@@ -7,7 +7,7 @@
  * Author URI:      https://awethemes.com
  * Text Domain:     awebooking
  * Domain Path:     /languages
- * Version:         3.2.23
+ * Version:         3.2.23.1
  *
  * @package         AweBooking
  */
